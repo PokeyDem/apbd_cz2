0c12d97023c5890ce74008e35b15e763ad9dd2ea
Aleksandr Maslov <s24439@pjwstk.edu.pl>
2024-03-15 15:24:04 +0100

    Change variable name in GetAverage method

--- apbd_cz2_app/apbd_cz2_app/Program.cs
+++ apbd_cz2_app/apbd_cz2_app/Program.cs
@@ -7,13 +7,13 @@ Console.WriteLine("Modyfikacja 3");
 
 static double GetAverage(int[] numbers)
 {
-    int sum = 0;
+    int sumAvg = 0;
     foreach (var number in numbers)
     {
-        sum += number;
+        sumAvg += number;
     }
 
-    return (double) sum / numbers.Length;
+    return (double) sumAvg / numbers.Length;
 }
 
 int[] numbers = {1,2,3,4};
